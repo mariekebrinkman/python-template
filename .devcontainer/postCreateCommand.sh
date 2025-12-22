1683de3647bad564ccc67b59c0b3b0bc1a58ac7f
Sander van Geest <49232516+vangeest@users.noreply.github.com>
2025-12-22 20:25:13 +0000

    final test

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
-pwd > dir.txt
-
-# Configure git settings
-git config pull.rebase false # needs to be configured before first pull or merge to prevent error
-git config merge.ours.driver true # configure git to use 'ours' merge strategy by default, this is not very git-like, but makes it easier voor studenten
-git config --global merge.conflictStyle diff3 # show source between ours and theirs in merge conflicts
+# Copy index.html to noVNC directory
+SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
+sudo cp "$SCRIPT_DIR/index.html" /usr/local/novnc/noVNC-*/index.html # change settings for novnc terminal, wildcard makes it independent of version
 
 # Install Python packages
 pip install pygame-ce
 pip install coderius-play
 
-# Copy index.html to noVNC directory
-sudo cp ./index.html /usr/local/novnc/noVNC-*/index.html # change settings for novnc terminal, wildcard makes it independent of version
+# Configure git settings
+git config pull.rebase false # needs to be configured before first pull or merge to prevent error
+git config merge.ours.driver true # configure git to use 'ours' merge strategy by default, this is not very git-like, but makes it easier voor studenten
+git config --global merge.conflictStyle diff3 # show source between ours and theirs in merge conflicts
