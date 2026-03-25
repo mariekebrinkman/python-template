c4a5ff57fd1c577b2e322a75f89f0ec366938763
mariekebrinkman <44111394+mariekebrinkman@users.noreply.github.com>
2026-03-25 16:33:11 +0000

    geel bewegend

--- play-main.py
+++ play-main.py
@@ -3,6 +3,8 @@
 
 import play
 
-play.new_circle()
+ball = play.new_circle(color="yellow")
+ball.start_physics(can_move=True, stable=False, x_speed=20, y_speed=10, 
+                   obeys_gravity=False, bounciness=0, mass=10, friction=0.1)
 
 play.start_program()
